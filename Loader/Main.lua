d4747b533786e97655e832ede5c079a3b54d4cea
NoctarionHub <izzibro29@gmail.com>
2026-07-07 02:39:22 +0700

    Update Main.lua

--- Loader/Main.lua
+++ Loader/Main.lua
@@ -1,1 +1,25 @@
+-- ============================================
+-- LOADER HYBRID - Simple tapi Modular
+-- ============================================
 
+local BASE_URL = "https://raw.githubusercontent.com/username/repo-name/main/"
+
+local Games = {
+    [83569851223739] = {
+        name = "+1 Speed Evolve",
+        main = "games/blox-fruits/main.lua",
+    },
+}
+
+local GameID = game.PlaceId
+local config = Games[GameID]
+
+if not config then
+    game.Players.LocalPlayer:Kick("Game not supported yet!")
+    return
+end
+
+print("🚀 Loading: " .. config.name)
+
+-- Load main
+loadstring(game:HttpGet(BASE_URL .. config.main))()
