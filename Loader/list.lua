dcc237ac6a0619338f5a380f2ca96c5d0d3b9174
NoctarionHub <izzibro29@gmail.com>
2026-07-12 02:43:12 +0700

    Update list.lua

--- Loader/list.lua
+++ Loader/list.lua
@@ -3,6 +3,7 @@ local Games = {
     [83569851223739] = "https://nhmain.vercel.app/Games/speed_evo/main.lua",
     [107654875426558] = "https://nhmain.vercel.app/Games/speed_evo/main.lua",
     [140544451091543] = "https://nhmain.vercel.app/Games/speed_evo/main.lua",
-    
+    [115681808123944] = "https://nhmain.vercel.app/Games/throw_coin/main.lua",
+    [72042130041700] = "https://nhmain.vercel.app/Games/throw_coin/main.lua",
 }
 return Games
